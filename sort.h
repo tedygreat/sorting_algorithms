85938df16166da51f143b0e4ebb0f8ac46439740
tedygreat <tedygreat@gmail.com>
2023-04-24 12:45:00 -0700

    task 0

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
-#ifndef "SORT_H"
-#define "SORT_H"
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,6 +17,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
