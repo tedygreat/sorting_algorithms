1a825ea4386ed84cc72255b804494301538b23df
tedygreat <tedygreat@gmail.com>
2023-04-25 02:57:46 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,11 @@
 #include "sort.h"
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 /**
  * bubble_sort - sort array lements from min to max value
  * @array: array
@@ -7,7 +14,7 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, index, tmp = 0;
+	size_t i, index;
 
 	if (array == NULL || size < 2)
 		return;
@@ -17,9 +24,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
+			swap_ints(array + index, array + index + 1);
 			print_array(array, size);
 			}
 		}
